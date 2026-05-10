16bf08965f74a17b20d7e2ce2064df3d7c7b7a3b
pmwensing <pmwensing1981@gmail.com>
2026-05-10 01:48:10 -0400

    Register v7.5.3 browser flow routes

--- src/socmint/wsgi.py
+++ src/socmint/wsgi.py
@@ -1,6 +1,8 @@
 from .dashboard import create_app
 from .full_report_alias import register_full_report_aliases
+from .full_report_browser import register_full_report_browser_flow
 
 
 app = create_app()
 register_full_report_aliases(app)
+register_full_report_browser_flow(app)
