4151ccadbbc437e2702f6c6d011560347564488b
pmwensing <pmwensing1981@gmail.com>
2026-06-14 20:57:26 -0400

    Register v23.0 case closure workspace routes

--- src/socmint/dossier_assembly_routes_v21_0.py
+++ src/socmint/dossier_assembly_routes_v21_0.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from flask import jsonify, redirect, render_template, request, session, url_for
 
+from .case_closure_routes_v23_0 import register_case_closure_routes_v23_0
 from .dossier_assembly_import_workspace_v21_1 import (
     build_dossier_assembly_workspace_v21_1,
     save_verified_dossier_arrangement,
@@ -161,4 +162,5 @@ def register_dossier_assembly_routes_v21_0(app):
     register_dossier_release_workspace_routes_v22_0(app)
     register_dossier_release_history_routes_v22_6(app)
     register_dossier_release_product_review_routes_v22_7(app)
+    register_case_closure_routes_v23_0(app)
     return app
