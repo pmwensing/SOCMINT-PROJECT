3eb74b6d2c8b1b11d304633cdf2cd1f2bb64cc58
pmwensing <pmwensing1981@gmail.com>
2026-06-18 05:00:36 -0400

    Register v28.6 platform operations routes

--- src/socmint/integration_admin_routes_v28_5.py
+++ src/socmint/integration_admin_routes_v28_5.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 from .connector_administration_routes_v28_5 import register_connector_administration_routes_v28_5
+from .platform_operations_routes_v28_6 import register_platform_operations_routes_v28_6
 
 
 def register_integration_admin_routes_v28_5(app):
     register_connector_administration_routes_v28_5(app)
+    register_platform_operations_routes_v28_6(app)
     return app
